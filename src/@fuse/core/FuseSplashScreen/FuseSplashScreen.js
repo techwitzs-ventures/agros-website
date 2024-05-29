0a80e48639c4cf12bb4009e13583ffc42eadf5c4
Anand Vardhan <anand.vardhan@conqudel.com>
2024-05-29 13:25:49 +0530

    logo is replaced by 'Agros Hub'

--- src/@fuse/core/FuseSplashScreen/FuseSplashScreen.js
+++ src/@fuse/core/FuseSplashScreen/FuseSplashScreen.js
@@ -1,11 +1,13 @@
 import { memo } from 'react';
 import Box from '@mui/material/Box';
+import config from '../../../app/configs/navigation-i18n/en';
 
 function FuseSplashScreen() {
   return (
     <div id="fuse-splash-screen">
       <div className="logo">
-        <img width="128" src="assets/images/logo/logo.svg" alt="logo" />
+        {/* <img width="128" src="assets/images/logo/logo.svg" alt="logo" /> */}
+        {config.APPLICATION_NAME}
       </div>
       <Box
         id="spinner"
