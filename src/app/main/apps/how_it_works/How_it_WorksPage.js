f4f96a6a87d54e04ea1f4b25c4693efca8d2d331
Anand Vardhan <anand.vardhan@conqudel.com>
2024-06-20 17:49:58 +0530

    sections are added in how it works page

--- src/app/main/apps/how_it_works/How_it_WorksPage.js
+++ src/app/main/apps/how_it_works/How_it_WorksPage.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import Section1 from './components/section1'
+import Section2 from './components/section2'
+import Section3 from './components/section3'
+import Section4 from './components/section4'
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "#e3ece5",
+    fontSize: "30px",
+    color: "#004b1c"
+  }
+}
 
 const HowItWorks = () => {
   return (
-    <div className='flex justify-center items-center'>How It Works Page....</div>
+    <div style={styles.container}>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+    </div>
   )
 }
 
---
+++ src/app/main/apps/how_it_works/components/section3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section3 = () => {
+  return (
+    <div style={{ height: "350px" }}>Section3</div>
+  )
+}
+
+export default Section3
